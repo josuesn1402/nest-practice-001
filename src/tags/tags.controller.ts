6399a655ef5f1e942e9120ed70fe806aef305650
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-06-21 19:53:06 -0500

    Modulo tags

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { TagsService } from './tags.service';
+import { Tag } from './tag.interface';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+  @Get()
+  getAll(): Tag[] {
+    return this.tagsService.getAll();
+  }
+}
